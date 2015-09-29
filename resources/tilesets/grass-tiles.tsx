741921948b8a9132ee092fd27e0955094b5fb229
Nathan <nathan.l.kramer@gmail.com>
2015-09-29 22:02:58 +1300

    Add tilesets and UI folder

---
+++ resources/tilesets/grass-tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="grass-tiles-2-small" tilewidth="32" tileheight="32" tilecount="72">
+ <image source="../spritesheets/tileset/grass-tiles-2-small.png" width="384" height="192"/>
+</tileset>
