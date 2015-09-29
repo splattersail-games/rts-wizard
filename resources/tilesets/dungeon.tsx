60e5849861d3f0e35d56d191fe1b9205dc934c96
Nathan <nathan.l.kramer@gmail.com>
2015-09-29 22:02:58 +1300

    Add tilesets and UI folder

---
+++ resources/tilesets/dungeon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="floors" tilewidth="32" tileheight="32" tilecount="128">
+ <image source="../spritesheets/tileset/floors.png" width="512" height="256"/>
+</tileset>
---
+++ resources/tilesets/tilesettest.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Tilesettest" tilewidth="32" tileheight="32" tilecount="9">
+ <image source="../spritesheets/Tilesettest.png" width="96" height="96"/>
+</tileset>
