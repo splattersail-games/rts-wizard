6ec8ed2fad9e002ce76a25ae6b6d1e827ba2e934
Nathan <nathan.l.kramer@gmail.com>
2015-10-11 20:51:53 +1300

    Start switching to lovetoys for entities

---
+++ engine-import.lua
@@ -0,0 +1,10 @@
+-- Importing lovetoys
+require 'src.libs.engine.Entity'
+require 'src.libs.engine.Engine'
+require 'src.libs.engine.System'
+require 'src.libs.engine.EventManager'
+require 'src.libs.engine.Component'
+
+-- Requiring all Events
+require 'src.libs.engine.events.ComponentAdded'
+require 'src.libs.engine.events.ComponentRemoved'
