b333b93ec1874f5abf2dbd88d6b0266b374f4c9e
Nathan Kramer <nathan.l.kramer@gmail.com>
2016-04-27 19:20:44 +1200

    Fix indentation

--- src/engine/systems/events/SelectUnits.lua
+++ src/engine/systems/events/SelectUnits.lua
@@ -2,27 +2,26 @@
 SelectUnits = class("SelectUnits", System)
 
 function SelectUnits:fireEvent(evt)
-    print("Unit selection triggered. Targets: " .. #self.targets)
-    local initialDeselect = true
+  local initialDeselect = true
 
-    if evt ~= nil and evt.rectangle then
-        for id, gameObj in pairs(self.targets) do
-            local collidableData = gameObj:get("Collidable")
+  if evt ~= nil and evt.rectangle then
+    for id, gameObj in pairs(self.targets) do
+      local collidableData = gameObj:get("Collidable")
 
-            if (collidableData) then 
-                if collidableData.AABB:objectWithinSquare(evt.rectangle) then
-                    if not love.keyboard.isDown( "lshift" ) and initialDeselect then
-                        --Game:deselectObjects()
-                        initialDeselect = false
-                    end
-                    local selection = gameObj:get("PlayerControlled")
-                    selection.selected = true
-                end
-            end
+      if (collidableData) then
+        if collidableData.AABB:objectWithinSquare(evt.rectangle) then
+          if not love.keyboard.isDown( "lshift" ) and initialDeselect then
+            --Game:deselectObjects()
+            initialDeselect = false
+          end
+          local selection = gameObj:get("PlayerControlled")
+          selection.selected = true
         end
+      end
     end
+  end
 end
 
 function SelectUnits:requires()
-    return {"PlayerControlled", "Position", "Collidable"}
+  return {"PlayerControlled", "Position", "Collidable"}
 end
